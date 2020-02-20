84e9986ab1f3bf843c1f60190f20fe3bc56c3876
Cameron Sinko <csinko@gmail.com>
2020-02-20 16:00:22 -0500

    Add init functions for DMA and TIM8

--- mcu/io_master/mcu/stm32h743/src/io_dma.c
+++ mcu/io_master/mcu/stm32h743/src/io_dma.c
@@ -1,6 +1,30 @@
 #include "io_dma.h"
 #include "main.h"
 #include "mcu_conf.h"
+#include "stm32h7xx_hal.h"
+
+IOM_ERROR InitDMA(void) {
+  __HAL_RCC_DMA2_CLK_ENABLE();
+
+  hdma_dma_generator0.Instance = DMA2_Stream7;
+  hdma_dma_generator0.Init.Request = DMA_REQUEST_TIM8_CH4;
+  hdma_dma_generator0.Init.Direction = DMA_MEMORY_TO_PERIPH;
+  hdma_dma_generator0.Init.PeriphInc = DMA_PINC_DISABLE;
+  hdma_dma_generator0.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_dma_generator0.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_dma_generator0.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+  hdma_dma_generator0.Init.Mode = DMA_NORMAL;
+  hdma_dma_generator0.Init.Priority = DMA_PRIORITY_VERY_HIGH;
+  hdma_dma_generator0.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+  hdma_dma_generator0.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_1QUARTERFULL;
+  hdma_dma_generator0.Init.MemBurst = DMA_MBURST_SINGLE;
+  hdma_dma_generator0.Init.PeriphBurst = DMA_PBURST_SINGLE;
+__HAL_LINKDMA(&htim8, hdma[TIM_DMA_ID_CC4], hdma_dma_generator0);
+  HAL_DMA_Init(htim8.hdma[TIM_DMA_ID_CC4]);
+  HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
+  return IOM_OK;
+}
 
 void StartDMATransfer(IOM_Output_Buffer* pBuffer) {
   HAL_DMA_Start(htim8.hdma[TIM_DMA_ID_CC4], (uint32_t)pBuffer->data, (uint32_t)(&((IO_PIN_GPIO_OUTPUT_PORT)->ODR)) + IO_PIN_GPIO_OUTPUT_OFFSET, pBuffer->length);
